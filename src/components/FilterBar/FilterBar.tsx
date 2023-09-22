0b1120cecb788778d4f57ae395198b5fac5aa812
asantoro36 <axelsantoro@hotmail.com>
2023-09-22 01:02:55 -0300

    make spike of filters

--- src/components/FilterBar/FilterBar.tsx
+++ src/components/FilterBar/FilterBar.tsx
@@ -12,8 +12,26 @@ import {
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import FilterListIcon from '@mui/icons-material/FilterList';
+import {useState} from "react";
+import {Service} from "../../Entities/Service";
 
-export default function FilterBar() {
+interface FilterBarProps {
+    services: Service[];
+    setFilteredServices: (filteredClasses: Service[]) => void;
+}
+
+const filtersStatus = new Map([
+    ['individual', false],
+    ['group', false],
+    ['unique', false],
+    ['weekly', false],
+    ['monthly', false],
+    ['regular', false],
+    ['good', false],
+    ['veryGood', false]
+]);
+
+export const FilterBar:  React.FC<FilterBarProps>  = ({ services, setFilteredServices}) => {
 
     const categories = [
         "Tutorías Escolares",
@@ -26,14 +44,20 @@ export default function FilterBar() {
         "Diseño Gráfico"
     ]
 
+    const applyFilters = () => {
+        const filteredServices = services.filter((service: Service) => {
+            return service.type.toLowerCase() === "individual"})
+        setFilteredServices(filteredServices)
+    }
+
     return (
         <Stack className={"filters"} spacing={2}>
             <div><Typography variant={"h6"} className="filter-title"><FilterListIcon/>Filtros</Typography></div>
             <div>
                 <Typography variant="subtitle2" className="filter-subtitle-font">Tipo:</Typography>
                 <FormGroup>
-                    <FormControlLabel control={<Checkbox />} label="Individual" />
-                    <FormControlLabel control={<Checkbox />} label="Grupal" />
+                    <FormControlLabel control={<Checkbox onChange={(e) => {applyFilters()} }/>} label="Individual" />
+                    <FormControlLabel control={<Checkbox onChange={(e) => {filtersStatus.set('group', e.target.checked)} }/>} label="Grupal" />
                 </FormGroup>
             </div>
             <Divider />
