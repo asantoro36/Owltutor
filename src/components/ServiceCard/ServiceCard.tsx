7f6d163cb13d1a6556d3ff091f1d38222afe702f
asantoro36 <59813325+asantoro36@users.noreply.github.com>
2023-09-24 23:38:52 -0300

    Add new service card styles

--- src/components/ServiceCard/ServiceCard.tsx
+++ src/components/ServiceCard/ServiceCard.tsx
@@ -5,11 +5,14 @@ import Typography from '@mui/material/Typography';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import EventIcon from '@mui/icons-material/Event';
 import PeopleIcon from '@mui/icons-material/People';
-import PersonIcon from '@mui/icons-material/Person';
+import DateRangeIcon from '@mui/icons-material/DateRange';
 import "./ServiceCard.css"
 import Button from '@mui/material/Button';
-import {CardHeader} from "@mui/material";
+import {CardHeader, Divider} from "@mui/material";
 import {Service} from "../../Entities/Service";
+import Avatar from "@mui/material/Avatar";
+import {red} from "@mui/material/colors";
+import GradeIcon from '@mui/icons-material/Grade';
 
 interface ServiceCardProps {
     service: Service;
@@ -31,30 +34,44 @@ export default function ServiceCard(props: ServiceCardProps) {
     }
 
     return (
-        <Card sx={{ maxWidth: 300 }} elevation={8}>
+        <Card sx={{ maxWidth: 400 }} elevation={8}>
             <CardHeader
             title={service.title}
-            style={{ paddingBottom: '4px' }}>
-
-            </CardHeader>
+            subheader= {service.description}/>
             <CardContent style={{ paddingTop: '4px' }}>
-                <div className="card-content">
-                    <div>
-                        <Typography variant="body2" color="text.secondary">
-                            {service.description}
-                        </Typography>
+                <div>
+                    <div className={'class-info'}>
+                        <div className={'class-info-items'}>
+                            <Typography className="description-points" color="textSecondary"><AccessTimeIcon/>{service.duration}</Typography>
+                            <Typography className="description-points" color="textSecondary"><EventIcon/>{setFrequency(service.frequency)}</Typography>
+                            <Typography className="description-points" color="textSecondary"><PeopleIcon/>{setType(service.type)}</Typography>
+                        </div>
+                        <div className={'class-info-items'} >
+                            <div style={{display:'flex', gap: 5, alignItems: "center"}}>
+                            <Typography className="description-points" color="textSecondary"><DateRangeIcon/></Typography>
+                            <Typography className="description-points" color="textSecondary">Lun</Typography>
+                            <Typography className="description-points" color="textSecondary">Mar</Typography>
+                            <Typography className="description-points" color="textSecondary">Mie</Typography>
+                            <Typography className="description-points" color="textSecondary">Jue</Typography>
+                            <Typography className="description-points" color="textSecondary">Vie</Typography>
+                            </div>
+                            <Typography className="description-points" color="textSecondary"><GradeIcon/>{service.rating}</Typography>
+                        </div>
                     </div>
-                    <div className="divider"/>
                     <div>
-                        <Typography className="description-points" color="textSecondary"><AccessTimeIcon/>{service.duration}</Typography>
-                        <Typography className="description-points" color="textSecondary"><EventIcon/>{setFrequency(service.frequency)}</Typography>
-                        <Typography className="description-points" color="textSecondary"><PeopleIcon/>{setType(service.type)}</Typography>
-                        <Typography className="description-points" color="textSecondary"><PersonIcon/>{service.responsible}</Typography>
+                        <div className={'responsible-name'}>
+                            <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">R</Avatar>
+                            <Typography variant="body2" color="text.secondary">{service.responsible}</Typography>
+                        </div>
+                        <Typography variant="body2" color="text.secondary">{service.responsibleExperience}</Typography>
                     </div>
                 </div>
                 <div className="card-footer">
-                        <span><Typography variant="h5"> ${service.cost} </Typography></span>
-                        <Button variant="contained" size="small">Contactar</Button>
+                    <Divider/>
+                    <div className="footer-content">
+                            <span style={{display: 'flex'}}><Typography variant="h5"> ${service.cost} </Typography><Typography className="cost-info" color="textSecondary"> p/clase</Typography></span>
+                            <Button variant="contained" size="small">Contactar</Button>
+                    </div>
                 </div>
             </CardContent>
         </Card>
