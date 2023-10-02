cf877cb90f343ff045fd1c23a8eeb9aa67f5cbb7
asantoro36 <59813325+asantoro36@users.noreply.github.com>
2023-10-02 00:37:25 -0300

    Remove form

--- src/login/LoginForm.tsx
+++ src/login/LoginForm.tsx
@@ -3,6 +3,7 @@ import {TextField} from "@mui/material";
 import "./Login.css"
 import Typography from "@mui/material/Typography";
 import {useNavigate} from "react-router-dom";
+import {getUser} from "../controller/UserController";
 export const LoginForm = () => {
     const navigate = useNavigate()
     const [formData, setFormData] = useState({
@@ -18,51 +19,50 @@ export const LoginForm = () => {
         });
     };
 
-    const handleSubmit = (event: any) => {
-        event.preventDefault();
-        console.log(formData);
+    const handleSubmit = () => {
+        console.log(getUser(formData.username)?.name);
+        console.log(getUser(formData.username));
     };
 
     return (
         <div className="login-form">
-            <form onSubmit={handleSubmit}>
-                <h3 className={"title-form"}>Inicia sesión</h3>
-                <div className={"login-input-form"}>
-                    <TextField
-                        variant="filled"
-                        margin="normal"
-                        label="Nombre de usuario o correo electrónico"
-                        name="username"
-                        value={formData.username}
-                        onChange={handleInputChange}
-                        required
-                    />
-                    <TextField
-                        variant="filled"
-                        margin="normal"
-                        label="Contraseña"
-                        name="password"
-                        type="password"
-                        value={formData.password}
-                        onChange={handleInputChange}
-                        required
-                    />
-                    <Typography className={"forget-pass"}><a href={"/password_recover"}>Olvidé mi contraseña</a></Typography>
+            <h3 className={"title-form"}>Inicia sesión</h3>
+            <div className={"login-input-form"}>
+                <TextField
+                    variant="filled"
+                    margin="normal"
+                    label="Nombre de usuario o correo electrónico"
+                    name="username"
+                    value={formData.username}
+                    onChange={handleInputChange}
+                    required
+                />
+                <TextField
+                    variant="filled"
+                    margin="normal"
+                    label="Contraseña"
+                    name="password"
+                    type="password"
+                    value={formData.password}
+                    onChange={handleInputChange}
+                    required
+                />
+                <Typography className={"forget-pass"}><a href={"/password_recover"}>Olvidé mi contraseña</a></Typography>
+            </div>
+            <div className={"login-button-form-container"}>
+                <div
+                    className={"primary-button"}
+                    onClick={handleSubmit}
+                >
+                    Iniciar sesión
                 </div>
-                <div className={"login-button-form-container"}>
-                    <div
-                        className={"primary-button"}
-                    >
-                        Iniciar sesión
-                    </div>
-                    <div
-                        className={"button-secondary"}
-                        onClick={() => navigate("/create")}
-                    >
-                        Crear cuenta
-                    </div>
+                <div
+                    className={"button-secondary"}
+                    onClick={() => navigate("/create")}
+                >
+                    Crear cuenta
                 </div>
-            </form>
+            </div>
         </div>
     )
 }
