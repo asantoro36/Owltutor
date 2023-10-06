e32a73ccbc103ffb5365646cada34da0f769c2a7
asantoro36 <59813325+asantoro36@users.noreply.github.com>
2023-10-06 14:55:19 -0300

    Add profile tabs

--- src/profile/Profile.tsx
+++ src/profile/Profile.tsx
@@ -1,12 +1,44 @@
 import AppBar from "../components/AppBar/AppBar";
 import React from "react";
 import { Courses } from "../courses/Courses";
+import {Tab, Tabs} from "@mui/material";
+import Typography from "@mui/material/Typography";
 
 export const Profile = () => {
+    const [value, setValue] = React.useState('one');
+
+    const handleChange = (event: React.SyntheticEvent, newValue: string) => {
+        setValue(newValue);
+    };
     return(
         <>
             <AppBar/>
-            <Courses/>
+            <Tabs
+                value={value}
+                onChange={handleChange}
+                textColor="secondary"
+                indicatorColor="secondary"
+                aria-label="secondary tabs example"
+            >
+                <Tab value="one" label="Publicaciones" />
+                <Tab value="two" label="Item Two" />
+                <Tab value="three" label="Item Three" />
+            </Tabs>
+            <div>
+                {value === "one" && (
+                    <Courses/>
+                )}
+                {value === "two" && (
+                    <Typography component="div">
+                        Contenido de la pestaña dos
+                    </Typography>
+                )}
+                {value === "three" && (
+                    <Typography component="div">
+                        Contenido de la pestaña tres
+                    </Typography>
+                )}
+            </div>
         </>
     )
 }
