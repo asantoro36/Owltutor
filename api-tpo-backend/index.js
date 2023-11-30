104841459250fd8cb44411537edc0d4e4c0cd0bc
asantoro36 <axelsantoro@hotmail.com>
2023-11-30 12:19:18 -0300

    Add backend

--- api-tpo-backend/index.js
+++ api-tpo-backend/index.js
@@ -1,20 +1,60 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {Pool } = require('pg');
+const cors = require('cors');
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'api-tpo',
+    password: 'testing',
+    port: 5432,
+});
 
 const app = express();
+
 app.use(bodyParser.json());
+app.use(cors());
 
-app.get('/ping', (req, res) => {
-    res.send('pong');
+app.post('/auth/login', async (req, res) => {
+    const {email, password} = req.body;
+
+    const query = 'SELECT * FROM users WHERE email = $1 AND password = $2';
+    const result = await pool.query(query, [email, password]);
+
+    if (result.rows.length > 0) {
+        res.json({ status: 'success', message: 'Inicio de sesión exitoso', user: { email } });
+    } else {
+        res.status(401).json({ status: 'error', message: 'Credenciales incorrectas' });
+    }
+});
+app.post('/users', async (req, res) => {
+    try {
+        const {
+            name,
+            surname,
+            mail,
+            phone,
+            password,
+            title,
+            experience,
+            photoUrl,
+        } = req.body;
+
+        const query = `
+      INSERT INTO users (name, surname, email, phone, pass, title, experience, photoUrl)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+      RETURNING id;`;
+
+        const result = await pool.query(query, [name, surname, mail, phone, password, title, experience, photoUrl]);
+
+        res.status(201).json(result.rows[0]);
+    } catch (error) {
+        console.error('Error al insertar en la base de datos:', error);
+        res.status(500).json({ status: 'error', message: 'Error interno del servidor' });
+    }
 });
 
-app.post('/auth/login', (req, res) => {
-    const { email, password } = req.body;
-
-    // lógica de autenticación
-
-    res.json({ status: 'success', message: 'Inicio de sesión exitoso', user: { email } });
-});
 
 const PORT = 8080;
 app.listen(PORT, () => {
